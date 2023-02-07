248288eed3743cf20d9c137b5bebeb3a34fc1eda
Dom G <21165092+dgrisafe@users.noreply.github.com>
2023-02-06 19:24:53 -0500

    Assign directory architecture and formats file

---
+++ directories_pc.R
@@ -0,0 +1,9 @@
+# set directories
+
+# call home directories, hidden from Git
+source("directories_home.R") # dir_home <- 
+
+# assign project subfolders
+dir_data <- paste0(dir_home, "\\0 Data")
+# dir_programs <- # assigned in separate folder on local machine
+dir_reports <- paste0(dir_home, "\\3 Reports")
