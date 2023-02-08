63556348b583c7bea36d500eb20cf032e0415349
Dom G <21165092+dgrisafe@users.noreply.github.com>
2023-02-07 21:03:15 -0500

    Begun data wrangling

--- !workflow.R
+++ !workflow.R
@@ -8,9 +8,9 @@ library(markdown)
 source("directories_pc.R")
 source("formats.R")
 
-## Rmd analysis
+## RMarkdown for Data Wrangling of raw .csv data file
 rmarkdown::render(
-  input = paste0(dir_programs, "\\HRI Grand Canyon.Rmd"),
+  input = paste0(dir_programs, "\\GrCa HRI Data Wrangling.Rmd"),
   output_format = "html_document",
   output_dir = dir_reports
   )
