6a5a30ed677e79f222d0bd7186390c3353c5bf1c
Dom G <21165092+dgrisafe@users.noreply.github.com>
2023-02-14 19:07:19 -0500

    Save R DataSet and prepare to define heat illness outcome variable

--- directories_pc.R
+++ directories_pc.R
@@ -5,5 +5,6 @@ source("directories_home.R") # dir_home <-
 
 # assign project subfolders
 dir_data <- paste0(dir_home, "\\0 Data")
+
 # dir_programs <- # assigned in separate folder on local machine
 dir_reports <- paste0(dir_home, "\\3 Reports")
