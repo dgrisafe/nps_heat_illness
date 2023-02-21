e375bbd5d67361978b847e25fc6b017699d519e5
Dom G <21165092+dgrisafe@users.noreply.github.com>
2023-02-21 13:14:53 -0500

    Organize folders Add subfolder for data from sharepoint

--- directories_pc.R
+++ directories_pc.R
@@ -3,8 +3,14 @@
 # call home directories, hidden from Git
 source("directories_home.R") # dir_home <- 
 
-# assign project subfolders
+# data folder
 dir_data <- paste0(dir_home, "\\0 Data")
 
+# sharepoint subfolder with data segments
+dir_data_sharepoint <- paste0(dir_data, "\\OneDrive_3_2-21-2023")
+
+# program/file folder
 # dir_programs <- # assigned in separate folder on local machine
+
+# output HTML reports folder
 dir_reports <- paste0(dir_home, "\\3 Reports")
